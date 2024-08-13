c0f4326893925be04b246af1def5d466f980cda0
masajinobe-ef <priscilla.effects@gmail.com>
2024-08-13 11:21:20 +0500

    github parser

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GithubController } from './github/github.controller';
+import { GithubService } from './github/github.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, GithubController],
+  providers: [AppService, GithubService],
 })
 export class AppModule {}
